cdeb72c2f36da7936a71990f7a648dd051375618
Jeff Harris <jefftharris@users.noreply.github.com>
2010-05-20 20:52:21 -0400

    Close buildId

---
+++ src/com/jefftharris/passwdsafe/PasswdFileData.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (©) 2009-2010 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jefftharris.passwdsafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.pwsafe.lib.exception.EndOfFileException;
+import org.pwsafe.lib.exception.InvalidPassphraseException;
+import org.pwsafe.lib.exception.UnsupportedFileVersionException;
+import org.pwsafe.lib.file.PwsField;
+import org.pwsafe.lib.file.PwsFile;
+import org.pwsafe.lib.file.PwsFileFactory;
+import org.pwsafe.lib.file.PwsFileV1;
+import org.pwsafe.lib.file.PwsFileV2;
+import org.pwsafe.lib.file.PwsFileV3;
+import org.pwsafe.lib.file.PwsRecord;
+import org.pwsafe.lib.file.PwsRecordV1;
+import org.pwsafe.lib.file.PwsRecordV2;
+import org.pwsafe.lib.file.PwsRecordV3;
+
+public class PasswdFileData
+{
+    public File itsFile;
+    public PwsFile itsPwsFile;
+    private final HashMap<String, PwsRecord> itsRecordsByUUID =
+        new HashMap<String, PwsRecord>();
+
+    private static final String TAG = "PasswdFileData";
+
+    private static final int FIELD_UNSUPPORTED = -1;
+    private static final int FIELD_NOT_PRESENT = -2;
+
+    public PasswdFileData(File file, StringBuilder passwd)
+        throws Exception
+    {
+        itsFile= file;
+        loadFile(passwd);
+   }
+
+    public void close()
+    {
+        itsFile= null;
+        itsPwsFile.dispose();
+        itsRecordsByUUID.clear();
+    }
+
+    public HashMap<String, PwsRecord> getRecordsByUUID()
+    {
+        return itsRecordsByUUID;
+    }
+
+    public PwsRecord getRecord(String uuid)
+    {
+        return itsRecordsByUUID.get(uuid);
+    }
+
+    public final String getEmail(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.EMAIL);
+    }
+
+    public final String getGroup(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.GROUP);
+    }
+
+    public final String getNotes(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.NOTES);
+    }
+
+    public final String getPassword(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.PASSWORD);
+    }
+
+    public final String getPasswdExpiryTime(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.PASSWORD_LIFETIME);
+    }
+
+    public final String getTitle(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.TITLE);
+    }
+
+    public final String getUsername(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.USERNAME);
+    }
+
+    public final String getURL(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.URL);
+    }
+
+    public final String getUUID(PwsRecord rec)
+    {
+        return getField(rec, PwsRecordV3.UUID);
+    }
+
+    private final String getField(PwsRecord rec, int fieldId)
+    {
+        if (itsPwsFile == null) {
+            return "";
+        }
+
+        switch (itsPwsFile.getFileVersionMajor())
+        {
+        case PwsFileV3.VERSION:
+        {
+            break;
+        }
+        case PwsFileV2.VERSION:
+        {
+            switch (fieldId)
+            {
+            case PwsRecordV3.GROUP:
+            {
+                fieldId = PwsRecordV2.GROUP;
+                break;
+            }
+            case PwsRecordV3.NOTES:
+            {
+                fieldId = PwsRecordV2.NOTES;
+                break;
+            }
+            case PwsRecordV3.PASSWORD:
+            {
+                fieldId = PwsRecordV2.PASSWORD;
+                break;
+            }
+            case PwsRecordV3.PASSWORD_LIFETIME:
+            {
+                fieldId = PwsRecordV2.PASSWORD_LIFETIME;
+                break;
+            }
+            case PwsRecordV3.TITLE:
+            {
+                fieldId = PwsRecordV2.TITLE;
+                break;
+            }
+            case PwsRecordV3.USERNAME:
+            {
+                fieldId = PwsRecordV2.USERNAME;
+                break;
+            }
+            case PwsRecordV3.UUID:
+            {
+                fieldId = PwsRecordV2.UUID;
+                break;
+            }
+            case PwsRecordV3.EMAIL:
+            case PwsRecordV3.URL:
+            {
+                fieldId = FIELD_NOT_PRESENT;
+                break;
+            }
+            }
+            break;
+        }
+        case PwsFileV1.VERSION:
+        {
+            switch (fieldId)
+            {
+            case PwsRecordV3.NOTES:
+            {
+                fieldId = PwsRecordV1.NOTES;
+                break;
+            }
+            case PwsRecordV3.PASSWORD:
+            {
+                fieldId = PwsRecordV1.PASSWORD;
+                break;
+            }
+            case PwsRecordV3.TITLE:
+            {
+                fieldId = PwsRecordV1.TITLE;
+                break;
+            }
+            case PwsRecordV3.USERNAME:
+            {
+                fieldId = PwsRecordV1.USERNAME;
+                break;
+            }
+            case PwsRecordV3.UUID:
+            {
+                // No real UUID field for V1, so just use the title which
+                // should be unique
+                fieldId = PwsRecordV1.TITLE;
+                break;
+            }
+            case PwsRecordV3.EMAIL:
+            case PwsRecordV3.GROUP:
+            case PwsRecordV3.PASSWORD_LIFETIME:
+            case PwsRecordV3.URL:
+            {
+                fieldId = FIELD_NOT_PRESENT;
+                break;
+            }
+            }
+            break;
+        }
+        default:
+        {
+            fieldId = FIELD_UNSUPPORTED;
+            break;
+        }
+        }
+
+        switch (fieldId)
+        {
+        case FIELD_UNSUPPORTED:
+        {
+            return "(unsupported)";
+        }
+        case FIELD_NOT_PRESENT:
+        {
+            return null;
+        }
+        default:
+        {
+            PwsField field = rec.getField(fieldId);
+            if (field == null) {
+                return null;
+            }
+            return field.toString();
+        }
+        }
+    }
+
+    private void loadFile(StringBuilder passwd)
+        throws IOException, NoSuchAlgorithmException,
+            EndOfFileException, InvalidPassphraseException,
+            UnsupportedFileVersionException
+    {
+        PasswdSafeApp.dbginfo(TAG, "before load file");
+        itsPwsFile = PwsFileFactory.loadFile(itsFile.getAbsolutePath(), passwd);
+        passwd.delete(0, passwd.length());
+        passwd = null;
+        PasswdSafeApp.dbginfo(TAG, "after load file");
+
+        Iterator<PwsRecord> recIter = itsPwsFile.getRecords();
+        while (recIter.hasNext()) {
+            PwsRecord rec = recIter.next();
+            String uuid = getUUID(rec);
+            if (uuid != null) {
+                itsRecordsByUUID.put(uuid, rec);
+            }
+        }
+
+        PasswdSafeApp.dbginfo(TAG, "file loaded");
+    }
+
+}
---
+++ src/com/jefftharris/passwdsafe/PasswdSafe.java
@@ -0,0 +1,410 @@
+/*
+ * Copyright (©) 2009-2010 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jefftharris.passwdsafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.pwsafe.lib.file.PwsRecord;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ExpandableListActivity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+
+public class PasswdSafe extends ExpandableListActivity {
+    private static final String TAG = "PasswdSafe";
+
+    private static final int DIALOG_GET_PASSWD = 0;
+    private static final int DIALOG_PROGRESS = 1;
+
+    private static final String RECORD = "record";
+    private static final String TITLE = "title";
+    private static final String GROUP = "group";
+
+    private static final String NO_GROUP_GROUP = "Records";
+
+    private File itsFile;
+    private ActivityPasswdFile itsPasswdFile;
+    private LoadTask itsLoadTask;
+    private boolean itsGroupRecords = true;
+    private boolean itsIsSortCaseSensitive = true;
+
+    private final ArrayList<Map<String, String>> itsGroupData =
+        new ArrayList<Map<String, String>>();
+    private final ArrayList<ArrayList<HashMap<String, Object>>> itsChildData =
+        new ArrayList<ArrayList<HashMap<String, Object>>>();
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        PasswdSafeApp.dbginfo(TAG, "onCreate intent:" + getIntent());
+        itsFile = new File(getIntent().getData().getPath());
+
+        PasswdSafeApp app = (PasswdSafeApp)getApplication();
+
+        SharedPreferences prefs =
+            PreferenceManager.getDefaultSharedPreferences(this);
+        itsGroupRecords = PasswdSafeApp.getGroupRecordsPref(prefs);
+        itsIsSortCaseSensitive = PasswdSafeApp.getSortCaseSensitivePref(prefs);
+
+        itsPasswdFile = app.accessPasswdFile(itsFile, this);
+        if (!itsPasswdFile.isOpen()) {
+            showDialog(DIALOG_GET_PASSWD);
+        } else {
+            showFileData();
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onDestroy()
+     */
+    @Override
+    protected void onDestroy()
+    {
+        PasswdSafeApp.dbginfo(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onPause()
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        removeDialog(DIALOG_GET_PASSWD);
+        if (itsLoadTask != null)
+            itsLoadTask.cancel(true);
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onResume()
+     */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        itsPasswdFile.touch();
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        removeDialog(DIALOG_GET_PASSWD);
+        removeDialog(DIALOG_PROGRESS);
+        super.onSaveInstanceState(outState);
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreateDialog(int)
+     */
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+        Dialog dialog = null;
+        switch (id) {
+        case DIALOG_GET_PASSWD:
+        {
+            LayoutInflater factory = LayoutInflater.from(this);
+            final View passwdView =
+                factory.inflate(R.layout.passwd_entry, null);
+
+            // TODO: click Ok when enter pressed
+            AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                .setTitle("Open " + itsFile.getName())
+                .setMessage("Enter password:")
+                .setView(passwdView)
+                .setPositiveButton("Ok",
+                                   new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        EditText passwdInput = (EditText) passwdView
+                            .findViewById(R.id.passwd_edit);
+                        openFile(
+                            new StringBuilder(passwdInput.getText().toString()));
+                    }
+                })
+                .setNegativeButton("Cancel",
+                                   new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        cancelFileOpen();
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener()
+                {
+                    public void onCancel(DialogInterface dialog)
+                    {
+                        cancelFileOpen();
+                    }
+                });
+            dialog = alert.create();
+            break;
+        }
+        case DIALOG_PROGRESS:
+        {
+            ProgressDialog dlg = new ProgressDialog(this);
+            dlg.setMessage("Loading...");
+            dlg.setIndeterminate(true);
+            dialog = dlg;
+            break;
+        }
+        }
+        return dialog;
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.ExpandableListActivity#onChildClick(android.widget.ExpandableListView, android.view.View, int, int, long)
+     */
+    @Override
+    public boolean onChildClick(ExpandableListView parent,
+                                View v,
+                                int groupPosition,
+                                int childPosition,
+                                long id)
+    {
+        PasswdFileData fileData = itsPasswdFile.getFileData();
+
+        PwsRecord rec = (PwsRecord)
+            itsChildData.get(groupPosition).
+            get(childPosition).
+            get(RECORD);
+
+        Uri.Builder builder = getIntent().getData().buildUpon();
+        String uuid = fileData.getUUID(rec);
+        if (uuid != null) {
+            builder.appendQueryParameter("rec", uuid.toString());
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, builder.build(),
+                                   this, RecordView.class);
+        startActivity(intent);
+        return true;
+    }
+
+    private void openFile(StringBuilder passwd)
+    {
+        removeDialog(DIALOG_GET_PASSWD);
+        showDialog(DIALOG_PROGRESS);
+        itsLoadTask = new LoadTask(passwd);
+        itsLoadTask.execute();
+    }
+
+    private void showFileData()
+    {
+        PasswdFileData fileData = itsPasswdFile.getFileData();
+        HashMap<String, PwsRecord> records = fileData.getRecordsByUUID();
+        RecordMapComparator comp =
+            new RecordMapComparator(itsIsSortCaseSensitive);
+
+        if (itsGroupRecords) {
+            TreeMap<String, ArrayList<PwsRecord>> recsByGroup;
+            if (itsIsSortCaseSensitive) {
+                recsByGroup = new TreeMap<String, ArrayList<PwsRecord>>();
+            } else {
+                recsByGroup = new TreeMap<String, ArrayList<PwsRecord>>(
+                                String.CASE_INSENSITIVE_ORDER);
+            }
+
+            for (Map.Entry<String, PwsRecord> entry: records.entrySet()) {
+                PwsRecord rec = entry.getValue();
+                String group = fileData.getGroup(rec);
+                if (group == null) {
+                    group = NO_GROUP_GROUP;
+                }
+                ArrayList<PwsRecord> groupList = recsByGroup.get(group);
+                if (groupList == null) {
+                    groupList = new ArrayList<PwsRecord>();
+                    recsByGroup.put(group, groupList);
+                }
+                groupList.add(rec);
+            }
+
+            for (Map.Entry<String, ArrayList<PwsRecord>> entry :
+                recsByGroup.entrySet()) {
+                Map<String, String> groupInfo =
+                    Collections.singletonMap(GROUP, entry.getKey());
+                itsGroupData.add(groupInfo);
+
+                ArrayList<HashMap<String, Object>> children =
+                    new ArrayList<HashMap<String, Object>>();
+                for (PwsRecord rec : entry.getValue()) {
+                    HashMap<String, Object> recInfo =
+                        new HashMap<String, Object>();
+                    String title = fileData.getTitle(rec);
+                    if (title == null) {
+                        title = "Untitled";
+                    }
+                    recInfo.put(TITLE, title);
+                    recInfo.put(RECORD, rec);
+                    children.add(recInfo);
+                }
+                Collections.sort(children, comp);
+                itsChildData.add(children);
+            }
+        } else {
+            Map<String, String> groupInfo =
+                Collections.singletonMap(GROUP, NO_GROUP_GROUP);
+            itsGroupData.add(groupInfo);
+
+            ArrayList<HashMap<String, Object>> children =
+                new ArrayList<HashMap<String, Object>>();
+            for (Map.Entry<String, PwsRecord> entry: records.entrySet()) {
+                PwsRecord rec = entry.getValue();
+                HashMap<String, Object> recInfo = new HashMap<String, Object>();
+                String title = fileData.getTitle(rec);
+                if (title == null) {
+                    title = "Untitled";
+                }
+                recInfo.put(TITLE, title);
+                recInfo.put(RECORD, rec);
+                children.add(recInfo);
+            }
+            Collections.sort(children, comp);
+            itsChildData.add(children);
+        }
+
+        ExpandableListAdapter adapter =
+            new SimpleExpandableListAdapter(PasswdSafe.this,
+                                            itsGroupData,
+                                            android.R.layout.simple_expandable_list_item_1,
+                                            new String[] { GROUP },
+                                            new int[] { android.R.id.text1 },
+                                            itsChildData,
+                                            android.R.layout.simple_expandable_list_item_1,
+                                            new String[] { TITLE },
+                                            new int[] { android.R.id.text1 });
+        setListAdapter(adapter);
+
+        if (itsGroupData.size() == 1) {
+            getExpandableListView().expandGroup(0);
+        }
+    }
+
+    private final void cancelFileOpen()
+    {
+        removeDialog(DIALOG_PROGRESS);
+        removeDialog(DIALOG_GET_PASSWD);
+        finish();
+    }
+
+
+    private final class LoadTask extends AsyncTask<Void, Void, Object>
+    {
+        private final StringBuilder itsPasswd;
+
+        private LoadTask(StringBuilder passwd)
+        {
+            itsPasswd = passwd;
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#doInBackground(Params[])
+         */
+        @Override
+        protected Object doInBackground(Void... params)
+        {
+            try {
+                return new PasswdFileData(itsFile, itsPasswd);
+            } catch (Exception e) {
+                return e;
+            }
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#onCancelled()
+         */
+        @Override
+        protected void onCancelled()
+        {
+            PasswdSafeApp.dbginfo(TAG, "LoadTask cancelled");
+            itsLoadTask = null;
+            cancelFileOpen();
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+        @Override
+        protected void onPostExecute(Object result)
+        {
+            if (isCancelled()) {
+                onCancelled();
+                return;
+            }
+            PasswdSafeApp.dbginfo(TAG, "LoadTask post execute");
+            dismissDialog(DIALOG_PROGRESS);
+            itsLoadTask = null;
+            if (result instanceof PasswdFileData) {
+                itsPasswdFile.setFileData((PasswdFileData)result);
+                showFileData();
+            } else if (result instanceof Exception) {
+                Exception e = (Exception)result;
+                String str;
+                if ((e instanceof IOException) &&
+                                (e.getMessage().equals("Invalid password")))
+                    str = e.getMessage();
+                else
+                    str = e.toString();
+                PasswdSafeApp.showFatalMsg(str, PasswdSafe.this);
+            }
+        }
+    }
+
+    private static final class RecordMapComparator implements
+                    Comparator<HashMap<String, Object>>
+    {
+        private boolean itsIsSortCaseSensitive;
+
+        public RecordMapComparator(boolean sortCaseSensitive)
+        {
+            itsIsSortCaseSensitive = sortCaseSensitive;
+        }
+
+        public int compare(HashMap<String, Object> arg0,
+                           HashMap<String, Object> arg1)
+        {
+            String title0 = arg0.get(TITLE).toString();
+            String title1 = arg1.get(TITLE).toString();
+
+            if (itsIsSortCaseSensitive) {
+                return title0.compareTo(title1);
+            } else {
+                return title0.compareToIgnoreCase(title1);
+            }
+        }
+    }
+}
